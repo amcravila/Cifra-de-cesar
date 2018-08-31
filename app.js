f05f0771358f73df876c7ca1349c098ea7225341
amcravila <amcravila@gmail.com>
2018-08-31 09:45:26 -0300

    Refatoração

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-function caesarCipher () {
-  
-  var phrase = prompt ("Digite uma frase:");
+$(document).ready(function() {
+  $("#submit-btn").click(caesarCipher);
+  $('#input-text').keyup(blockNumber);
+});
 
-  if (phrase === '') {
-    return alert ('Digite uma frase!');
-  } 
-
-  for(i=0; i < phrase.length; i++){
-    var numbers = '0123456789';
-    if (numbers.indexOf(phrase.charAt(i),0) !== -1){
-      return alert ('Digite uma frase utilizando apenas letras!');
+function blockNumber(event){
+    const regex = new RegExp("^[a-z]+$");
+    let key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)){
+        event.preventDefault();
+        return false;
     }
-  }   
+}
 
-  var option = parseInt(prompt("O que deseja fazer? \n1- Criptografar \n2- Descriptografar")); 
+function caesarCipher(text) {
+  //
+  // for(i=0; i < phrase.length; i++){
+  //   let numbers = '0123456789';
+  //   if (numbers.indexOf(phrase.charAt(i),0) !== -1){
+  //     return alert ('Digite uma frase utilizando apenas letras!');
+  //   }
+  // }
+
+  var option = parseInt(prompt("O que deseja fazer? \n1- Criptografar \n2- Descriptografar"));
 
   if (option === 1) {
     cipher();
@@ -24,31 +32,29 @@ function caesarCipher () {
   }
 
   function cipher() {
-    var encrypted = '';
+    let encrypted = '';
     for (var i in phrase) {
-      var character = phrase.charCodeAt(i);
+      let character = phrase.charCodeAt(i);
       if (phrase.toUpperCase() === phrase) {
         encrypted += String.fromCharCode((character - 65 + 7) % 26 + 65);
       } else {
         encrypted += String.fromCharCode((character - 97 + 7) % 26 + 97);
       }
-    }   
+    }
     alert ('Frase Criptografada: ' + encrypted);
   }
 
   function decipher() {
-    var decrypted = '';
+    let decrypted = '';
     for (var i in phrase) {
-      var character = phrase.charCodeAt(i);
+      let character = phrase.charCodeAt(i);
       if (phrase.toUpperCase() === phrase) {
         decrypted += String.fromCharCode((character - 65 - 7 + 26) % 26 + 65);
       } else {
         decrypted += String.fromCharCode((character - 97 - 7 + 26) % 26 + 97);
       }
-    } 
+    }
     alert ('Frase Descriptografada: ' +  decrypted);
   }
 
-}
-
-caesarCipher ();
+};
